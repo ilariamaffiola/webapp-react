fcd465bfa59c6d11b3cf61e3617beec5543da6f4
Ilaria <maffiolailaria1@gmail.com>
2025-05-30 16:19:36 +0200

    {UPD} fix NotFoundPage layout and improve styling; add route in App for 404 handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import DefaultLayout from "./layout/DefaultLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import MoviePage from "./pages/MoviePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
           <Route element={<DefaultLayout />}>
             <Route index element={<Homepage />}></Route>
             <Route path="/movies/:id" element={<MoviePage />}></Route>
+            <Route path="*" element={<NotFoundPage />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <>
-      <div className="d-flex flex column-align-items-center justify-content-center">
+      <div className="d-flex flex-column align-items-center justify-content-center">
         <h2>Page not Found</h2>
-        <p>Non è stato possibile caricare la pagina</p>
+        <p className="mt-3">Non è stato possibile caricare la pagina</p>
         <Link to="/" className="btn btn-primary">
           Torna alla Home
         </Link>
