2e47c04f99839f0e10e5ca56b5791e00e81ff887
Ilaria <maffiolailaria1@gmail.com>
2025-05-30 16:34:21 +0200

    {UPD} enhance Homepage layout by adding movie card structure

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,18 @@ const Homepage = () => {
   return (
     <>
       <h1 className="text-primary">Movie List</h1>
-      <div className="row"></div>
+      <div className="row">
+        <div className="col-lg-4 col-md-6 col-12">
+          <div className="card">
+            <div className="card-image-top">
+              <img src="https://picsum.photos/200/300" alt="Movie" />
+            </div>
+            <div className="card-body">
+              <h3 className="text-primary"></h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
