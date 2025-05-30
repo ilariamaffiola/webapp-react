852aa533a943ea5d62abb437d18c752c1f70eeb1
Ilaria <maffiolailaria1@gmail.com>
2025-05-30 17:06:04 +0200

    {UPD} enhance Homepage and MoviePage components by adding movie data structure and improving layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+const InitialMovies = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    relase_year: "2010",
+    abstract:
+      "A skilled thief is given a chance at redemption if he can successfully perform inception.",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    genre: "Crime",
+    relase_year: "1972",
+    abstract: "ul Italian-American crime family and their struggles.",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 3,
+    title: "Titanic",
+    director: "James Cameron",
+    genre: "Romance",
+    relase_year: "1997",
+    abstract:
+      "A romantic story set against the tragic sinking of the RMS Titanic.",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 4,
+    title: "The Matrix",
+    director: "The Wachowskis",
+    genre: "Action",
+    relase_year: "1999",
+    abstract:
+      "A hacker discovers the truth about his reality and his role in the war against its controllers.",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 5,
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    relase_year: "2014",
+    abstract:
+      "A team of explorers travels through a wormhole in space to save humanity.",
+    image: "https://picsum.photos/200/300",
+  },
+];
 const Homepage = () => {
   return (
     <>
@@ -26,66 +78,6 @@ const Homepage = () => {
               </div>
             </div>
           </Link>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="card">
-            <div className="card-image-top d-flex justify-content-center">
-              <img
-                className="img-fluid "
-                src="https://picsum.photos/200/300"
-                alt="Movie"
-              />
-            </div>
-            <div className="card-body">
-              <h3 className="text-primary">Titolo</h3>
-              <h4>
-                <em>director</em>
-              </h4>
-              <p>genere</p>
-              <p>anno di rilascio</p>
-              <p>trama</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="card">
-            <div className="card-image-top d-flex justify-content-center">
-              <img
-                className="img-fluid "
-                src="https://picsum.photos/200/300"
-                alt="Movie"
-              />
-            </div>
-            <div className="card-body">
-              <h3 className="text-primary">Titolo</h3>
-              <h4>
-                <em>director</em>
-              </h4>
-              <p>genere</p>
-              <p>anno di rilascio</p>
-              <p>trama</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="card">
-            <div className="card-image-top d-flex justify-content-center">
-              <img
-                className="img-fluid "
-                src="https://picsum.photos/200/300"
-                alt="Movie"
-              />
-            </div>
-            <div className="card-body">
-              <h3 className="text-primary">Titolo</h3>
-              <h4>
-                <em>director</em>
-              </h4>
-              <p>genere</p>
-              <p>anno di rilascio</p>
-              <p>trama</p>
-            </div>
-          </div>
         </div>
       </div>
     </>
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -4,10 +4,21 @@ const MoviePage = () => {
   return (
     <>
       <div className="row">
-        <div className="col-12 col-md-6 col-lg-4">Immagine</div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <img
+            className="img-fluid "
+            src="https://picsum.photos/200/300"
+            alt="Movie"
+          />
+        </div>
         <div className="col-12 col-md-6 col-lg-8">
-          <h1>Titolo</h1>
-          <p>Altre informazioni</p>
+          <h1 className="text-primary">Titolo</h1>
+          <h4>
+            <em>director</em>
+          </h4>
+          <p>genere</p>
+          <p>anno di rilascio</p>
+          <p>trama</p>
         </div>
       </div>
     </>
