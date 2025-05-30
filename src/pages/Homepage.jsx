bf21f0fcb8e9ea2b7f97fec2015eac6d827b9db2
Ilaria <maffiolailaria1@gmail.com>
2025-05-30 17:29:40 +0200

    {UPD} enhance Homepage component by implementing movie data mapping and improving card layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const InitialMovies = [
+const initialMovies = [
   {
     id: 1,
     title: "Inception",
@@ -53,32 +54,31 @@ const InitialMovies = [
   },
 ];
 const Homepage = () => {
+  const [movies, setMovies] = useState(initialMovies);
   return (
     <>
       <h1 className="text-primary my-4">Movie List</h1>
       <div className="row g-4">
-        <div className="col-lg-4 col-md-6 col-12">
-          <Link to="/movies/1" className="text-decoration-none">
-            <div className="card">
-              <div className="card-image-top d-flex justify-content-center">
-                <img
-                  className="img-fluid "
-                  src="https://picsum.photos/200/300"
-                  alt="Movie"
-                />
+        {movies.map((movie) => (
+          <div className="col-lg-4 col-md-6 col-12">
+            <Link to={`/movies/${movie.id}`} className="text-decoration-none">
+              <div className="card">
+                <div className="card-image-top d-flex justify-content-center">
+                  <img className="img-fluid " src={movie.image} alt="Movie" />
+                </div>
+                <div className="card-body">
+                  <h3 className="text-primary">{movie.title}</h3>
+                  <h4>
+                    <em>{movie.director}</em>
+                  </h4>
+                  <p>{movie.genre}</p>
+                  <p>{movie.relase_year}</p>
+                  <p>{movie.abstract}</p>
+                </div>
               </div>
-              <div className="card-body">
-                <h3 className="text-primary">Titolo</h3>
-                <h4>
-                  <em>director</em>
-                </h4>
-                <p>genere</p>
-                <p>anno di rilascio</p>
-                <p>trama</p>
-              </div>
-            </div>
-          </Link>
-        </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </>
   );
