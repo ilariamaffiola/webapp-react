5bba8216e28993ff2f41af3bcdef97b4f74b732a
Ilaria <maffiolailaria1@gmail.com>
2025-05-30 16:12:00 +0200

    [ADD] feat: implement NotFoundPage component for handling 404 errors

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const NotFoundPage = () => {
+  return (
+    <>
+      <div className="d-flex flex column-align-items-center justify-content-center">
+        <h2>Page not Found</h2>
+        <p>Non è stato possibile caricare la pagina</p>
+        <Link to="/" className="btn btn-primary">
+          Torna alla Home
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
