c8d641e011d6657519edafa10267edb46cc80d17
Ilaria <maffiolailaria1@gmail.com>
2025-06-03 16:44:52 +0200

    [UPD] update package.json and package-lock.json to add cors dependency and improve movie fetching in Homepage component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,60 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
+import axios from "axios";
+
 const Homepage = () => {
-  const initialMovies = [
-    {
-      id: 1,
-      title: "Inception",
-      director: "Christopher Nolan",
-      genre: "Science Fiction",
-      relase_year: "2010",
-      abstract:
-        "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 2,
-      title: "The Godfather",
-      director: "Francis Ford Coppola",
-      genre: "Crime",
-      relase_year: "1972",
-      abstract: "ul Italian-American crime family and their struggles.",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 3,
-      title: "Titanic",
-      director: "James Cameron",
-      genre: "Romance",
-      relase_year: "1997",
-      abstract:
-        "A romantic story set against the tragic sinking of the RMS Titanic.",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 4,
-      title: "The Matrix",
-      director: "The Wachowskis",
-      genre: "Action",
-      relase_year: "1999",
-      abstract:
-        "A hacker discovers the truth about his reality and his role in the war against its controllers.",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      id: 5,
-      title: "Interstellar",
-      director: "Christopher Nolan",
-      genre: "Science Fiction",
-      relase_year: "2014",
-      abstract:
-        "A team of explorers travels through a wormhole in space to save humanity.",
-      image: "https://picsum.photos/200/300",
-    },
-  ];
-  const [movies, setMovies] = useState(initialMovies);
+  const [movies, setMovies] = useState([]);
+  const fetchBooks = () => {
+    axios
+      .get("http://127.0.0.1:3000/api/movies/")
+      .then((resp) => {
+        setMovies(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <>
